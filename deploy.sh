761e4d6c5095a29cd7077a1feb9939d7532e828c
ninawangyimei <nina_wangyimei@163.com>
2024-02-28 22:09:41 +0800

    增加部署文件

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh deploy.sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# set NODE_OPTIONS=--openssl-legacy-provider
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd .vitepress/dist
+
+# 拷贝目录和文件
+cp -r ../../.github ./
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:ninawangyimei/yirengongsi.git master:gh-pages
+
+cd -
